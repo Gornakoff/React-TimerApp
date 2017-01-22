949b1d1b28935811ac98a300852bf8a02cbec9ce
Gornakoff <nikolaygornakov@yahoo.com>
2017-01-22 02:56:32 +0200

    Add static controls component

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -1,8 +1,9 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 let expect = require('expect');
-let TestUtils = require('react-addons-test-utils');
 let $ = require('jQuery');
+let TestUtils = require('react-addons-test-utils');
+
 
 let Countdown = require('Countdown');
 
